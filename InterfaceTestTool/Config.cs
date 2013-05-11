a09a24847cf4a8ceb468e75ac59ffe6476d695bc
Steve <Paynter@yans.net>
2013-05-10 17:50:25 -0700

    Skeletons and Stubs
    Start of the design

---
+++ InterfaceTestTool/Config.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InterfaceTestTool
+{
+    class Config
+    {
+        string ConfigName;
+        string ConfigPath;
+        List<string> NameList = new List<string>();
+        List<string> ValueList = new List<string>();
+
+        public void LoadValues()
+        {
+
+        }
+
+        public void GetValue(string name)
+        {
+
+        }
+    }
+}
---
+++ InterfaceTestTool/Form_Main.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace InterfaceTestTool
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Start = new System.Windows.Forms.Button();
+            this.listView_InterfaceList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(404, 282);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(75, 23);
+            this.button_Start.TabIndex = 0;
+            this.button_Start.Text = "Start";
+            this.button_Start.UseVisualStyleBackColor = true;
+            // 
+            // listView_InterfaceList
+            // 
+            this.listView_InterfaceList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView_InterfaceList.FullRowSelect = true;
+            this.listView_InterfaceList.GridLines = true;
+            this.listView_InterfaceList.Location = new System.Drawing.Point(12, 12);
+            this.listView_InterfaceList.Name = "listView_InterfaceList";
+            this.listView_InterfaceList.Size = new System.Drawing.Size(467, 264);
+            this.listView_InterfaceList.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView_InterfaceList.TabIndex = 1;
+            this.listView_InterfaceList.UseCompatibleStateImageBehavior = false;
+            this.listView_InterfaceList.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Status";
+            this.columnHeader1.Width = 78;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Number";
+            this.columnHeader2.Width = 57;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Name";
+            this.columnHeader3.Width = 268;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Batch ID";
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 317);
+            this.Controls.Add(this.listView_InterfaceList);
+            this.Controls.Add(this.button_Start);
+            this.Name = "Form_Main";
+            this.Text = "TIDAL Test";
+            this.Load += new System.EventHandler(this.Form_Main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.ListView listView_InterfaceList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
